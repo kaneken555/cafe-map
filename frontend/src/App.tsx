6f675d93992445e04986d8592e47e629fea39a3f
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-03-14 07:44:05 +0900

    [add]Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
