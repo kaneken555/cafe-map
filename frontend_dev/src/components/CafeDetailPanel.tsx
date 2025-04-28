e5564836755bacdb79ba4ba3d1b6c53d31e0201a
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-04-29 08:14:16 +0900

    [add]CloseButtonコンポーネント化

--- frontend_dev/src/components/CafeDetailPanel.tsx
+++ frontend_dev/src/components/CafeDetailPanel.tsx
@@ -1,6 +1,7 @@
 // components/CafeDetailPanel.tsx
 import React from "react";
 import CafeDetailCard from "./CafeDetailCard";
+import CloseButton from "./CloseButton";
 import { Cafe } from "../api/mockCafeData"; // 👈 Cafe 型を import
 
 
@@ -13,29 +14,28 @@ interface CafeDetailPanelProps {
 }
 
 const CafeDetailPanel: React.FC<CafeDetailPanelProps> = ({ cafe, onClose, selectedMap, myCafeList, setMyCafeList }) => {
+  const renderCafeDetailCard = () => {
+    if (!cafe) return null;
+    return (
+      <CafeDetailCard
+        cafe={cafe}
+        selectedMap={selectedMap}
+        myCafeList={myCafeList}
+        setMyCafeList={setMyCafeList}
+      />
+    );
+  };
+
   return (
     <div
       className={`fixed left-0 top-16 h-[calc(100vh-4rem)] w-[400px] bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 overflow-y-auto
         ${cafe ? "translate-x-0" : "-translate-x-full"}`}
     >
       <div className="p-1">
-        {/* 閉じるボタン */}
-        <button
-          onClick={onClose}
-          className="absolute top-0 right-3 text-lg font-bold text-gray-600 hover:text-black"
-        >
-          ×
-        </button>
+        <CloseButton onClick={onClose} />
       </div>
 
-      {cafe && (
-        <CafeDetailCard
-          cafe={cafe}
-          selectedMap={selectedMap}
-          myCafeList={myCafeList || []} // ←ここで空配列にしてあげると安全
-          setMyCafeList={setMyCafeList} 
-          />
-      )}
+      {renderCafeDetailCard()}
     </div>
   );
 };
