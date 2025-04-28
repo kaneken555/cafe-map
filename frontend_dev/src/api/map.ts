2addc33ecc6d2232ad4bf6534f55d788bb7d34a3
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-04-28 09:25:46 +0900

    [add]map.ts

---
+++ frontend_dev/src/api/map.ts
@@ -0,0 +1,43 @@
+// src/api/map.ts
+import axios from "axios";
+import { getCsrfToken } from "./auth";
+
+interface CreateMapRequest {
+    name: string;
+  }
+
+export const createMap = async (params: CreateMapRequest): Promise<void> => {
+    console.log("📡 マップ作成リクエスト:", params.name);
+
+    const csrfToken = await getCsrfToken(); // CSRF トークンを取得
+
+    try {
+        const response = await axios.post(`http://localhost:8000/api/maps/`, 
+        { name: params.name }, 
+        { 
+            headers: {
+            "X-CSRFToken": csrfToken,
+            },
+            withCredentials: true ,
+        }, // クッキーを送信する
+        );
+        return response.data;
+    } catch (error) {
+        console.error("createMap エラー:", error);
+        throw error;
+    } 
+}
+
+export const getMapList = async (): Promise<any> => {
+    try {
+        const response = await axios.get(`http://localhost:8000/api/maps/`, {
+            withCredentials: true,
+        });
+        console.log("📡 マップ一覧取得リクエスト:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("getMapList エラー:", error);
+        throw error;
+    }
+};
+
