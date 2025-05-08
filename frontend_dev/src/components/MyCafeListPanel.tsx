d04ae4bbcbd1c4b237faf56e38ef57892b9182f5
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-05-08 18:57:55 +0900

    [add]SidePanelLayoutコンポーネント追加

--- frontend_dev/src/components/MyCafeListPanel.tsx
+++ frontend_dev/src/components/MyCafeListPanel.tsx
@@ -1,8 +1,8 @@
 // components/MyCafeListPanel.tsx
 import React from "react";
-import { Cafe } from "../api/mockCafeData"; // ✅ Cafe型をインポート
-import CloseButton from "./CloseButton";
+import { Cafe } from "../api/mockCafeData";
 import CafeListItem from "./CafeListItem";
+import SidePanelLayout from "./SidePanelLayout";
 
 interface MyCafeListPanelProps {
   isOpen: boolean;
@@ -12,36 +12,27 @@ interface MyCafeListPanelProps {
 }
 
 
-const MyCafeListPanel: React.FC<MyCafeListPanelProps> = ({ isOpen, onClose, cafes, onCafeClick }) => {
+const MyCafeListPanel: React.FC<MyCafeListPanelProps> = ({ 
+  isOpen, 
+  onClose, 
+  cafes, 
+  onCafeClick, 
+}) => {
   return (
-    <div
-      className={`fixed top-16 left-0 h-[calc(100vh-4rem)] w-[400px] bg-white shadow-lg z-40 transform transition-transform duration-300 ease-in-out ${
-        isOpen ? "translate-x-0" : "-translate-x-full"
-      }`}
-    >
-      <div className="h-full p-3 relative flex flex-col">
-        {/* 閉じるボタン */}
-        <div className="p-1">
-          <CloseButton onClick={onClose} />
-        </div>
-        <div className="text-2xl font-bold mb-4">My Café List</div>
-          <input
-            type="text"
-            placeholder="キーワードを入力"
-            className="w-full mb-4 px-3 py-2 border rounded focus:outline-none"
-          />
-        {/* カフェリスト */}
-        <div className="flex-1 overflow-y-auto pr-1">
-          {cafes.length === 0 ? (
-            <p className="text-gray-500 text-sm">このマップにカフェは登録されていません。</p>
-          ) : (
-            cafes.map((cafe, i) => (
-              <CafeListItem key={i} cafe={cafe} onClick={onCafeClick} />
-            ))
-          )}
-        </div>
+    <SidePanelLayout isOpen={isOpen} onClose={onClose} title="My Café List">
+      <input
+        type="text"
+        placeholder="キーワードを入力"
+        className="w-full mb-4 px-3 py-2 border rounded focus:outline-none"
+      />
+      <div className="flex-1 overflow-y-auto pr-1">
+        {cafes.length === 0 ? (
+          <p className="text-gray-500 text-sm">このマップにカフェは登録されていません。</p>
+        ) : (
+          cafes.map((cafe, i) => <CafeListItem key={i} cafe={cafe} onClick={onCafeClick} />)
+        )}
       </div>
-    </div>
+    </SidePanelLayout>
   );
 };
 
