1d5db6622d7ae1f025fade56d3746aca456bd0c4
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-05-30 21:25:00 +0900

    [add]ログイン状態復元

--- frontend_dev/src/App.tsx
+++ frontend_dev/src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import './App.css'
-import { useState } from 'react';
+// import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import { Toaster } from "react-hot-toast";
@@ -11,6 +11,8 @@ import { MapProvider } from "./contexts/MapContext";
 import { CafeProvider } from './contexts/CafeContext';
 import { GroupProvider } from './contexts/GroupContext';
 
+import InitializeApp from './components/InitializeApp'; // ✅ 初期化ロジックを切り出し
+
 const App: React.FC = () => {
   // const [user, setUser] = useState<{ id: number; name: string } | null>(null);
 
@@ -20,18 +22,19 @@ const App: React.FC = () => {
         <MapProvider>
           <CafeProvider>
             <GroupProvider>
-            <div className="min-h-screen flex flex-col">       
-              {/* トーストを追加 */}
-              <Toaster /> 
+              <InitializeApp /> {/* ✅ 初期化ロジックを切り出し */}
+                <div className="min-h-screen flex flex-col">       
+                  {/* トーストを追加 */}
+                  <Toaster /> 
 
-              {/* メインコンテンツ */}
-              <main className="flex-grow">
-                <Routes>
-                  {/* <Route path="/" element={<HomePage user={user} setUser={setUser} />} /> */}
-                  <Route path="/" element={<HomePage />} />
-                </Routes>
-              </main>
-            </div>
+                  {/* メインコンテンツ */}
+                  <main className="flex-grow">
+                    <Routes>
+                      {/* <Route path="/" element={<HomePage user={user} setUser={setUser} />} /> */}
+                      <Route path="/" element={<HomePage />} />
+                    </Routes>
+                  </main>
+                </div>
             </GroupProvider>
           </CafeProvider>
         </MapProvider>
