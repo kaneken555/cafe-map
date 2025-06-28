ce01572f0f410fc8274e922e0750e6601beefde9
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-06-28 12:58:57 +0900

    [add]Nginx関連Dockerファイル

--- frontend_dev/src/components/ModalActionButton.tsx
+++ frontend_dev/src/components/ModalActionButton.tsx
@@ -10,7 +10,7 @@ interface ModalActionButtonProps {
 const ModalActionButton: React.FC<ModalActionButtonProps> = ({ label, onClick, icon }) => {
   return (
     <button
-      className="w-full py-3 bg-[#FFC800] hover:bg-[#D8A900] cursor-pointer text-black text-lg rounded-xl flex justify-center items-center space-x-2"
+      className="w-full py-3 bg-[#FFC800] hover:bg-[#D8A900] cursor-pointer text-black text-base sm:text-lg rounded-xl flex justify-center items-center space-x-2"
       onClick={onClick}
     >
       {icon && <span>{icon}</span>}
