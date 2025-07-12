29994c36c8959b098dff52886ec869d2ecea2238
kaneken <99122116+kaneken555@users.noreply.github.com>
2025-07-12 16:04:53 +0900

    [refactor] BaseModal適用 #61

--- frontend_dev/src/components/KeywordSearchModal.tsx
+++ frontend_dev/src/components/KeywordSearchModal.tsx
@@ -1,7 +1,7 @@
 // components/KeywordSearchModal.tsx
 import React, { useState } from "react";
-import CloseModalButton from "./CloseModalButton"; // 共通の閉じるボタンコンポーネント
-import { MODAL_STYLES } from "../constants/ui";
+import BaseModal from "./BaseModal"; // 共通のモーダルコンポーネント
+
 
 interface Props {
   onClose: () => void;
@@ -20,27 +20,23 @@ const KeywordSearchModal: React.FC<Props> = ({
   };
 
   return (
-    <div className={MODAL_STYLES.MAIN_MODAL.CONTAINER}>
-      <div className="bg-[#fffaf0] p-6 rounded-lg w-[400px] shadow-md relative">
+    <BaseModal isOpen={true} onClose={onClose} title="キーワード検索" size="md">
 
-        <CloseModalButton onClose={onClose} />
+      <input
+        type="text"
+        placeholder="カフェ名や地名など"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        className="w-full px-3 py-2 border rounded mb-4"
+      />
+      <button
+        onClick={handleSearch}
+        className="w-full bg-[#FFC800] text-black py-2 rounded hover:bg-[#D8A900] cursor-pointer"
+      >
+        検索
+      </button>
 
-        <h2 className={MODAL_STYLES.MAIN_MODAL.TITLE}>キーワード検索</h2>
-        <input
-          type="text"
-          placeholder="カフェ名や地名など"
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-          className="w-full px-3 py-2 border rounded mb-4"
-        />
-        <button
-          onClick={handleSearch}
-          className="w-full bg-[#FFC800] text-black py-2 rounded hover:bg-[#D8A900] cursor-pointer"
-        >
-          検索
-        </button>
-      </div>
-    </div>
+    </BaseModal>
   );
 };
 
